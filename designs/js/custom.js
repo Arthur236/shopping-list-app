4677d9ec2358ab3b882c85641c0fa80c84b6a621
Arthur236 <arthur.thungu@gmail.com>
2017-08-28 23:37:24 +0300

    (Chore) Started working on the edit_shopping_list ui design

--- designs/js/custom.js
+++ designs/js/custom.js
@@ -7,18 +7,20 @@ document.addEventListener("DOMContentLoaded", function () {
 		.fadeOut();
 });
 
-var counter = 0;
+let counter = 0;
+let items = new Array;
 
 function addShoppingListItem(divId) {
 
 	let newItem = document.createElement('div');
 
 	newItem.className = "input-field col s8";
-	newItem.innerHTML = "<label for='items[]'>Item " + (counter + 1) + "</label><input id='items[" + counter + "]' name='items[" + counter + "]' type='text' required='required'/>";
+	newItem.innerHTML = "<label for='items[" + counter + "]'>Item</label><input id='items[" + counter + "]' name='items[" + counter + "]' type='text' required='required'/>";
 
 	let icons = document.createElement('div');
+	let fxn = "enableInput('items[" + counter + "]')";
 	icons.className = "input-field col s4";
-	icons.innerHTML = "<span><a href='#' class='btn-floating'><i class='material-icons'>delete</i></a><a href='#' class='btn-floating'><i class='material-icons'>edit</i></a></span>";
+	icons.innerHTML = '<span><a href="#" class="btn-floating"><i class="material-icons">delete</i></a><a href="#" class="btn-floating" onclick="'+ fxn +'"><i class="material-icons">edit</i></a></span>';
 
 	document.getElementById(divId).appendChild(newItem);
 	document.getElementById(divId).appendChild(icons);
@@ -29,4 +31,19 @@ function addShoppingListItem(divId) {
 	foreach ($items as $item) {
 		 echo $eachInput . "<br>";
 	}*/
+}
+
+function deleteShoppingListItem(divId) {
+	var elem = document.getElementById('#'+divId);
+
+    elem.remove();
+    return false;
+}
+
+function enableInput(inputId) {
+	if ($("#"+inputId).attr("disabled")) {
+		$("#"+inputId).removeAttr("disabled");
+	} else {
+		$("#"+inputId).attr("disabled","disabled");
+	}
 }
