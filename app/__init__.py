483d5bc7a272bed35d1c6167b918951face5223d
Arthur236 <arthur.thungu@gmail.com>
2017-08-29 14:05:37 +0300

    (Chore) Setup Pylint

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,17 @@
+"""
+Code used to initialize the app module
+
+PEP8 demands that names of constants should be in all caps.
+Pylint, as PEP8, expects module level variables to be "constants".
+"""
 from flask import Flask
 
-# Initialize the app
-app = Flask(__name__, instance_relative_config=True)
-
-# Load the views
 from app import views
 
+# Initialize the app
+APP = Flask(__name__, instance_relative_config=True)
+
 # Load the config file
-app.config.from_object('config')
-app.static_folder = 'static'
+APP.config.from_object('config')
+
+APP.static_folder = 'static'
--- app/views.py
+++ app/views.py
@@ -1,27 +1,48 @@
+"""
+Code for rendering the views
+"""
 from flask import render_template
 
 from app import app
 
 @app.route('/')
 def index():
+    """
+    Render the index page
+    """
     return render_template("index.html")
 
 @app.route('/dashboard')
 def dashboard():
+    """
+    Render the user dashboard
+    """
     return render_template("dashboard.html")
 
 @app.route('/create-list')
 def create_list():
+    """
+    View used to create new shopping lists
+    """
     return render_template("create_shopping_list.html")
 
 @app.route('/edit-list')
 def edit_list():
+    """
+    View used to edit shopping lists
+    """
     return render_template("edit_shopping_list.html")
 
 @app.route('/view-list')
 def view_list():
+    """
+    View for displaying a particular shopping list and all its items
+    """
     return render_template("shopping_list.html")
 
 @app.route('/edit-item')
 def edit_item():
+    """
+    View for editing shopping list items
+    """
     return render_template("shopping_list_items.html")
